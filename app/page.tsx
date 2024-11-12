dc4eefcbf370e1d735b602532d981e6c17e0367e
Victor Li <victorli5674@yahoo.com>
2024-11-11 21:36:23 -0800

    setup nextjs, tailwind, shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/Button"
+
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <Button>Button</Button>
+    </>
+  )
+}
